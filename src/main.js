d01b7950811b49a9ad79753a6eb3b986dfd02dad
tangyali778 <63383047+tangyali778@users.noreply.github.com>
2020-05-17 13:32:08 +0800

    完成登录界面ui的渲染

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-// ,webpack会让main.js这个入口文件和.less建立依赖联系
+// webpack会让main.js这个入口文件和.less建立依赖联系
 // 在js中@表示src的意思
 // import '@/styles/base.less'
 
-import router from '@/router'
+import router from '@/router';
+
+// 导入elementui
+import '@/plugins/elementui.js'
 
 new Vue({
   render: h => h(App),
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,93 @@
 <template>
-  <diV>
-    我是登录页面
-  </diV>
+  <div class="login-container">
+    <div class="left">
+      <div class="title-box">
+        <img src="@/assets/login_icon.png" alt />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
+      </div>
+      <!-- form表单部分 -->
+      <el-form class="login-form">
+        <el-form-item>
+          <el-input placeholder="请输入手机号" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入密码" show-password prefix-icon="el-icon-lock"></el-input>
+        </el-form-item>
+          <el-form-item>
+            <el-row :gutter="18" >
+              <el-col :span="16">
+                   <el-input placeholder="请输入验证码" prefix-icon="el-icon-key"></el-input>
+              </el-col>
+               <el-col :span="8">
+                   <img class="captcha" src="http://47.106.148.205/heimamm/public/captcha?type=login&random=0.1115232862923281" alt="">
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-form-item>
+             <el-checkbox></el-checkbox> 我已阅读并同意<el-link type="primary" href="http://www.baidu.com">用户协议</el-link>和<el-link type="primary" href="http://www.baidu.com">隐私条款</el-link>
+          </el-form-item>
+           <el-form-item>
+             <el-button type="primary" style="width:100%">登录</el-button>
+          </el-form-item>
+          <el-form-item>
+             <el-button type="primary" style="width:100%">注册</el-button>
+          </el-form-item>
+      </el-form>
+    </div>
+    <div class="right">
+      <img src="@/assets/login_bg.png" alt />
+    </div>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.login-container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: linear-gradient(225deg, #1493fa, #01c6fa);
+  .left {
+    width: 478px;
+    height: 550px;
+    background: #f5f5f5;
+    padding: 48px;
+    .title-box {
+      display: flex;
+      align-items: center;
+      img {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+      .title {
+        font-size: 24px;
+        margin-right: 14px;
+      }
+      .line {
+        width: 1px;
+        background-color: #c7c7c7;
+        height: 27px;
+        margin-right: 12px;
+      }
+      .sub-title {
+        font-size: 22px;
+      }
+    }
+    .login-form {
+      margin-top: 29px;
+      .captcha {
+        width: 100%;
+        height: 40px;
+      }
+    }
+  }
+}
+</style>
 
