9b2d4907b31d752b2ba8a1cf01a93e7a008c94e1
tangyali778 <63383047+tangyali778@users.noreply.github.com>
2020-05-21 14:59:28 +0800

    18完成了用户列表中table的渲染

--- src/views/layout/user/index.vue
+++ src/views/layout/user/index.vue
@@ -2,14 +2,14 @@
   <div class="user">
     <!-- 搜索部分 -->
     <el-card>
-      <el-form inline  :model="searchForm" ref="searchRef" label-width="80px">
-        <el-form-item   label="用户名称">
+      <el-form inline :model="searchForm" ref="searchRef" label-width="80px">
+        <el-form-item label="用户名称">
           <el-input style="width:240px" v-model="searchForm.username"></el-input>
         </el-form-item>
-        <el-form-item  label="用户邮箱">
+        <el-form-item label="用户邮箱">
           <el-input style="width:240px" v-model="searchForm.email"></el-input>
         </el-form-item>
-        <el-form-item  label="角色">
+        <el-form-item label="角色">
           <el-select style="width:240px" v-model="searchForm.role_id" placeholder="请选择">
             <el-option
               v-for="item in options"
@@ -19,7 +19,7 @@
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item  style="margin-left:80px">
+        <el-form-item style="margin-left:80px">
           <el-button type="primary">搜索</el-button>
           <el-button>清除</el-button>
           <el-button type="primary">+ 新增用户</el-button>
@@ -27,13 +27,36 @@
       </el-form>
     </el-card>
     <!-- 内容部分 -->
-    <el-card style="margin-top:20px;"></el-card>
+    <el-card style="margin-top:20px;">
+      <!-- 表格 -->
+      <el-table :data="userList" stripe style="width: 100%">
+        <el-table-column type="index" label="序号" width="80"></el-table-column>
+        <el-table-column prop="username" label="用户名"></el-table-column>
+        <el-table-column prop="phone" label="电话"></el-table-column>
+        <el-table-column prop="email" label="邮箱"></el-table-column>
+        <el-table-column prop="role" label="角色"></el-table-column>
+        <el-table-column prop="remark" label="备注"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+           <span v-if="scope.row.status===0" style="color:red">禁用</span>
+           <span v-if="scope.row.status===1" style="color:#6ac144">启用</span>
+          </template>
+        </el-table-column>
+           <el-table-column label="操作" width="280">
+          <template slot-scope="scope">
+            <el-button type="primary">编辑</el-button>
+            <el-button  :type="scope.row.status===0? 'success':'info'">{{scope.row.status===0?'启用':'禁用'}}</el-button>
+            <el-button>删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "userList",
+  name: " userList",
   data() {
     return {
       // 模型
@@ -44,6 +67,8 @@ export default {
       },
       page: 1, // 查询时候的页码
       limit: 2, // 查询时候的页容量(每页查询多少条)
+      userList: [], //存放发送请求得到的用户信息
+      total: 0,
 
       options: [
         {
@@ -64,6 +89,31 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    //先发送请求得到用户信息
+    async getUserData() {
+      const res = await this.$axios.get("/user/list", {
+        params: {
+          // 这个就相当于...this.searchForm
+           //  username: this.searchForm.username,
+          //   email: this.searchForm.email,
+          //   role_id: this.searchForm.role_id
+          ...this.searchForm,
+          page: this.page,
+          limit: this.limit
+        }
+      });
+      //console.log(res)
+      if (res.data.code == 200) {
+        this.userList = res.data.data.items;
+        this.total = res.data.data.pagination.total;
+      }
+    }
+  },
+  created() {
+    // 获取用户列表数据,用户内容展示
+    this.getUserData();
   }
 };
 </script>
